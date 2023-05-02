98ccf3166d49fe2de1748df8a0bc96d157a9f680
SanketPotdar121695 <sanket.potdar563@gmail.com>
2023-05-03 01:19:49 +0530

    initial commit

--- Server/middlewares/auth.middleware.js
+++ Server/middlewares/auth.middleware.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
+const { secretKey } = require('../config/db');
 
 const auth = (req, res, next) => {
   let token = req.headers.token;
   if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, secretKey, (err, decoded) => {
       if (decoded.role == 'admin' || decoded.role == 'superadmin') {
         next();
       } else {
